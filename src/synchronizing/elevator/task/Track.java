d0437d997a1b589af627fb3db447768e90443ee8
Romyleads <romyleads@gmail.com>
2025-06-10 11:55:14 +0200

    add lesson 3 / Synchronizing

---
+++ src/synchronizing/elevator/task/Track.java
@@ -0,0 +1,24 @@
+package synchronizing.elevator.task;
+
+import synchronizing.elevator.model.Elevator;
+
+public class Track implements Runnable{
+    private int nRaces;
+    private int capacity;
+    private Elevator elevator;
+
+    public Track(int nRaces, int capacity, Elevator elevator) {
+        this.nRaces = nRaces;
+        this.capacity = capacity;
+        this.elevator = elevator;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i <nRaces ; i++) {
+            elevator.add(capacity);
+        }
+
+    }
+}
